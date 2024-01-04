57e4131b12e39eccbfc2a9061e94b9a63020ac7c
wb-jx721745 <wb-jx721745@alibaba-inc.com>
2024-01-04 18:36:51 +0800

    feat: 登录

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,8 +1,29 @@
-
+import * as bcrypt from 'bcryptjs';
 const helper = {
     uint8ArrayToUtf8(uint8Array) {
         const decoder = new TextDecoder('utf-8');
         return decoder.decode(uint8Array, { stream: true });
+    },
+    async hashPassword(password) {
+        const saltRounds = 10;
+        try {
+            const hash = await bcrypt.hash(password, saltRounds);
+            console.log('Password Hash:', hash);
+            return hash;
+        } catch (error) {
+            console.error('Error hashing password:', error);
+        }
+    },
+
+    async checkPassword(password, hash) {
+        try {
+            const match = await bcrypt.compare(password, hash);
+            console.log('Do passwords match?', match);
+            return match;
+        } catch (error) {
+            console.error('Error comparing password and hash:', error);
+        }
     }
+
 }
 export default helper;
--- app/router.ts
+++ app/router.ts
@@ -16,6 +16,6 @@ export default (app: Application) => {
   router.get('/model', controller.openAi.getModel);
   router.get('/chat', controller.openAi.createChatCompletion);
   router.post('/chat', controller.openAi.createChatCompletionPost);
-  router.get('/register', controller.user.register);
-  router.get('/login', controller.user.login);
+  router.post('/register', controller.user.register);
+  router.post('/login', controller.user.login);
 };
